464a67a13e55194440a38edc0758cae6b231bf75
KJWKJW <force1394@gmail.com>
2024-11-11 16:56:12 +0900

    feat: JWT 토큰 만료 여부 검증 로직 개발

--- src/main/java/com/teamps/auth_service/security/JwtTokenProvider.java
+++ src/main/java/com/teamps/auth_service/security/JwtTokenProvider.java
@@ -1,10 +1,8 @@
 package com.teamps.auth_service.security;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ClaimsBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -19,25 +17,50 @@ public class JwtTokenProvider {
     private String secretKey;
 
     @Value("${jwt.expiration}") // yml 에 설정한 토큰 만료시간 30분
-    private long validityMilliseconds;
+    private long validityInMilliseconds;
 
     // JWT 토큰 생성
     public String createToken(UserDetails userDetails) {
+        Claims claims = Jwts.claims().setSubject(userDetails.getUsername());
         Date now = new Date();
-        Date validity = new Date(now.getTime() + validityMilliseconds);
-        Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
-
-        Claims claims = Jwts.claims()
-                .subject(userDetails.getUsername())  // 사용자 이름
-                .issuedAt(now)  // 발행 시간
-                .expiration(validity)  // 만료 시간
-                .build();  // Claims 객체로 빌드
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
 
         return Jwts.builder()
-                .claims(claims)
-                .signWith(key)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
-        }
+    }
+
+
+    // JWT 토큰에서 사용자 이름 추출
+    public String getUsernameFromToken(String token) {
+        return parseClaims(token).getSubject(); // subject 로 사용자 이름 추출
+    }
+
+
+    // JWT 토큰 검증
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
+    // JWT 토큰 만료 여부 체크
+    private boolean isTokenExpired(String token) {
+        return parseClaims(token).getExpiration().before(new Date());
+    }
+
+
+    // 토큰에서 Claims 추출
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
 
 }

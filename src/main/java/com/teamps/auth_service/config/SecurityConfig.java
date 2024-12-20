f421e9a0985776d33f533cb478e2f9d791108a30
KJWKJW <force1394@gmail.com>
2024-12-21 04:36:35 +0900

    feat: Matchers config 설정 추가

--- src/main/java/com/teamps/auth_service/config/SecurityConfig.java
+++ src/main/java/com/teamps/auth_service/config/SecurityConfig.java
@@ -1,21 +1,38 @@
 package com.teamps.auth_service.config;
 
+import com.teamps.auth_service.util.AuthenticatedMatchers;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated()
-            )
+                // REST API
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AuthenticatedMatchers.matcherArray)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
             .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
---
+++ src/main/java/com/teamps/auth_service/util/AuthenticatedMatchers.java
@@ -0,0 +1,20 @@
+package com.teamps.auth_service.util;
+
+public class AuthenticatedMatchers {
+
+    private AuthenticatedMatchers() {}
+
+    public static final String[] matcherArray = {
+            // v3 설정 추가할 필요 있을 수 있음
+            "/",
+            "/auth/code/**",
+            "/auth/login/**",
+            "/auth/logout",
+            "/auth/reissue",
+            "/api-docs",
+            "/api-docs/**",
+            "/swagger-ui/index.html",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+}
